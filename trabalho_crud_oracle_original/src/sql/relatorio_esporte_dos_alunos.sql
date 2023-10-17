829cae4ebd872cd18ddf0f7a5b375febe86e7dd3
fracalove <148223727+fracalove@users.noreply.github.com>
2023-10-17 12:51:04 -0300

    Delete trabalho_crud_oracle_original directory

--- trabalho_crud_oracle_original/src/sql/relatorio_esporte_dos_alunos.sql
+++
@@ -1,6 +0,0 @@
--- junção de tabelas
-SELECT a.matricula, a.nome, a.cpf, a.id_esporte, c.nome, c.coordenador
-FROM AlunosEG a
-INNER JOIN EsporteEG c
-ON a.id_esporte = c.id_esporte
-ORDER BY a.matricula
